e552ddd15c53595e724f5b3fd0567ce04209bb2b
Mateo, Berrio Restrepo <mberriorestrepo@gmail.com>
2023-05-18 19:33:47 +0200

    add useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import confetti from 'canvas-confetti'
 
-import './App.css'
-import { Square } from './components/Square'
 import { TURNS } from './constants'
 import { WinnerModal } from './components/WinnerModal'
 import { checkEndGame, checkWinnerFrom } from './logic/board'
 import { Table } from './components/Table'
-import { resetGameStorage, saveGameToStorage } from './logic/storage'
+import { resetGameStorage } from './logic/storage'
+import { useLocalStorage } from './logic/storage/useLocalStorage'
 
 function App() {
-  const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board')
-    return boardFromStorage
-    ? JSON.parse(boardFromStorage)
-    : Array(9).fill(null)
-  })
+  // DE ESTA FORMA NO FUNCIONA EL LOCAL STORAGE EN REACT
+  // CREAMOS UN HOOK "useLocalStorage" PARA USAR EL LOCAL STORAGE
 
+  // const [board, setBoard] = useState(() => {
+  //   const boardFromStorage = window.localStorage.getItem('board')
+  //   return boardFromStorage
+  //   ? JSON.parse(boardFromStorage)
+  //   : Array(9).fill(null)
+  // })
+
+  // const [turn, setTurn] = useState(() => {
+  //   const turnFromStorage = window.localStorage.getItem('turn')
+  //   return turnFromStorage ?? TURNS.X
+  // })
+
+  const [board, setBoard] = useLocalStorage('board', Array(9).fill(null))
   // el turno inicial lo tien "X"
-  const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ?? TURNS.X
-  })
+  const [turn, setTurn] = useLocalStorage('turn', TURNS.X)
 
   // null si no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null)
+
+  // esto solo se ejecutará una vez en el ciclo de vida del componente
+  // o cada que cambien alguna dependencia en "[]"
+  // ejem: [winner] -> cada que cambia el winner se ejecutara este
+  // siempre hay que pasarle unas dependecias para 
+  // que no haga un loop, o ciclo infinito o en 
+  // su defecto un array vacio []
+  useEffect(() => {
+    if (checkWinnerFrom(board)) {
+      confetti()
+      setWinner(checkWinnerFrom(board))
+    } else if (checkEndGame(board)) {
+      setWinner(false)
+    }
+  }, [])
 
   const updateBoard = (index) => {
     // no actualizamos esta posición
@@ -39,10 +59,10 @@ function App() {
     setTurn(newTurn)
 
     // save game in storage
-    saveGameToStorage({
-      board: newBoard,
-      turn: newTurn
-    })
+    // saveGameToStorage({
+    //   board: newBoard,
+    //   turn: newTurn
+    // })
 
     const newWinner = checkWinnerFrom(newBoard)
 
