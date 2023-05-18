e76fc0493d2790052a38185ed7b5f75611546f33
Mateo, Berrio Restrepo <mberriorestrepo@gmail.com>
2023-05-19 00:17:23 +0200

    install linter and clear code

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Triqui - ReactJs
 
-npm install 
-npm run dev
+```
+npm install // instalar paquetes de node
+npm run dev // compilar para desarrollo
+npm install standard -D // instalar el linter
+npm test // ejecutar el linter
+npm run build // compilar para prod
+```
--- src/logic/storage/useLocalStorage.js
+++ src/logic/storage/useLocalStorage.js
@@ -1,23 +1,23 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 export function useLocalStorage (key, initialValue) {
-    const [storedValue, setStoredValue] = useState(() => {
-        try {
-            const item = window.localStorage.getItem(key)
-            return item ? JSON.parse(item) : initialValue
-        } catch (error) {
-            return initialValue
-        }
-    })
-
-    const setValue = value => {
-        try {
-            setStoredValue(value)
-            window.localStorage.setItem(key, JSON.stringify(value))
-        } catch (error) {
-            console.error(error)
-        }
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      return initialValue
     }
+  })
 
-    return [storedValue, setValue]
+  const setValue = value => {
+    try {
+      setStoredValue(value)
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return [storedValue, setValue]
 }
